715578fe9aa8743d744ab33a49874772cc1664f1
Rauan <asetovrauan0@gmail.com>
2025-11-18 12:36:33 +0500

    Project settings

---
+++ src/config/project.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    host: str = Field(alias="APP_HOST")
+    port: int = Field(alias="APP_PORT")
+    debug: bool = Field(default=False, alias="APP_DEBUG")
+    version: str = Field(alias="APP_VERSION")
+    hooks_enabled: bool = Field(default=True, alias="APP_HOOKS_ENABLED")
+    root_path: str = Field(default="", alias="APP_ROOT_PATH")
+    timezone_shift: int = Field(default=3, alias="APP_TIMEZONE_SHIFT")
+
+
+settings = Settings()
