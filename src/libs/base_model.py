2d79529b66603493eca18fc05b73a2ca9bd0151c
Rauan <asetovrauan0@gmail.com>
2025-11-18 13:03:43 +0500

    Base Model for SQLAlchemy ORM

---
+++ src/libs/base_model.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """Base Model for SqlAlchemy ORM"""
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
