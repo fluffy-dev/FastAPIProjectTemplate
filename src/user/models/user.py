5d92a4f566a7b5fe885f7427578d68207909b170
Rauan <asetovrauan0@gmail.com>
2025-11-19 11:41:15 +0500

    User Model

---
+++ src/user/models/user.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.libs.base_model import Base
+
+
+class UserModel(Base):
+    """ SQLAlchemy model for user """
+    __tablename__ = "users"
+
+    name: Mapped[str] = mapped_column(String(20))
+    surname: Mapped[str] = mapped_column(String(20), nullable=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    password: Mapped[str]
+    is_active: Mapped[bool] = mapped_column(default=False)
