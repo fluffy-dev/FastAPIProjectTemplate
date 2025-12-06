be6e5a8a33787f73e401555f390d7a7b28503805
Rauan <asetovrauan0@gmail.com>
2025-12-06 12:21:43 +0500

    added user model factory for testing

--- tests/factories.py
+++ tests/factories.py
@@ -1,15 +1,25 @@
 from polyfactory.factories.pydantic_factory import ModelFactory
+from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
+
 from src.auth.dto import RegistrationDTO, UserDTO, TokenDTO
 from src.auth.models.user import UserModel
 
+
 class RegistrationDTOFactory(ModelFactory[RegistrationDTO]):
-    """Factory for generating RegistrationDTO payloads."""
+    """Factory for generating RegistrationDTO payloads (Pydantic)."""
     __model__ = RegistrationDTO
 
+
 class UserDTOFactory(ModelFactory[UserDTO]):
-    """Factory for generating UserDTO objects."""
+    """Factory for generating UserDTO objects (Pydantic)."""
     __model__ = UserDTO
 
-class UserModelFactory(ModelFactory[UserModel]):
-    """Factory for generating SQLAlchemy User models."""
+
+class TokenDTOFactory(ModelFactory[TokenDTO]):
+    """Factory for generating TokenDTO objects (Pydantic)."""
+    __model__ = TokenDTO
+
+
+class UserModelFactory(SQLAlchemyFactory[UserModel]):
+    """Factory for generating SQLAlchemy User models (ORM)."""
     __model__ = UserModel
